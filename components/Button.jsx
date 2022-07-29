7a1229fc6766b83214bac535f6280c09703052bc
Ahmad Shiddiq <ahmadshiddiqamin@gmail.com>
2022-07-29 20:47:53 +0700

    fix: button style

--- components/Button.jsx
+++ components/Button.jsx
@@ -1,4 +1,15 @@
-import { buttonThemeFn } from "../pages/utils";
+function buttonThemeFn(value) {
+  switch (value) {
+    case "border-dark-blue":
+      return "border border-main-dark-blue bg-white hover:bg-main-dark-blue hover:text-white text-main-dark-blue hover:shadow-xl";
+    case "fill-blue":
+      return "bg-main-cyan hover:bg-main-pink text-white hover:shadow-[_0px_14px_43px_0px_rgba(255,129,142,0.4)]";
+    case "transparent-white":
+      return "bg-white/50 text-white hover:bg-white hover:text-main-dark-blue";
+    default:
+      return "";
+  }
+}
 
 export default function Button({ text, icon, theme, className, onClick }) {
   return (
