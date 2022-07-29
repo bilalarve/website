212420c043c38ec09b4fe4fe405d124600f888ed
Ahmad Shiddiq <ahmadshiddiqamin@gmail.com>
2022-07-29 16:38:35 +0700

    chore: finish homepage

--- components/Button.jsx
+++ components/Button.jsx
@@ -1,10 +1,12 @@
-export default function Button({ text, icon, theme }) {
+export default function Button({ text, icon, theme, className }) {
   function themeFn(value) {
     switch (value) {
       case "border-dark-blue":
         return "border border-main-dark-blue bg-white hover:bg-main-dark-blue hover:text-white text-main-dark-blue";
       case "fill-blue":
         return "bg-main-cyan hover:bg-main-pink text-white";
+      case "transparent-white":
+        return "bg-white/50 text-white hover:bg-white hover:text-main-dark-blue";
       default:
         return "";
     }
@@ -14,7 +16,7 @@ export default function Button({ text, icon, theme }) {
     <button
       className={`flex h-[42px] items-center gap-2.5 rounded-full px-6 transition-colors ${themeFn(
         theme
-      )}`}
+      )} ${className}`}
     >
       <span className="text-sm font-semibold">{text}</span>
       {icon}
--- pages/index.js
+++ pages/index.js
@@ -19,69 +19,78 @@ export default function Home() {
       <Header />
 
       <main>
-        <div className="wrapper grid grid-cols-12 items-center justify-between gap-5 pb-28 pt-32">
-          <div className="col-span-5 col-start-2">
-            <h1 className="mb-[34px] text-[64px] font-bold leading-tight text-main-dark-blue">
-              Hello! I Am
-              <br />
-              Bilal Arve.
-              <br />I Am an Illustrator
-              {/* <span className="rounded-full bg-main-purple py-3 px-6 text-2xl font-bold text-white">
-                Illustrator
-              </span> */}
-            </h1>
-            <p className="mb-[50px] max-w-[466px] text-base leading-relaxed text-main-gray">
-              Hey Im Muhammad Bilal Arve and i’m from Indonesia.I have been
-              working as an Illustrator and Graphic Designer with more than 3
-              years of experience and UI UX Designer for 1 years and still
-              learning and exploring.
-            </p>
-            <div className="mb-14 flex gap-[34px]">
-              <Button text="Lets Talk" theme="fill-blue" icon={<HiChat />} />
-              <Button
-                text="Resume"
-                theme="border-dark-blue"
-                icon={<HiClipboardList />}
-              />
-            </div>
-            <div className="flex gap-6">
-              <div>
-                <p className="mb-4 text-base font-medium text-main-gray">
-                  Projects Completed
-                </p>
-                <p className="text-4xl font-bold text-main-dark-blue">300+</p>
-              </div>
-              <div className="h-20 border-l-2 border-main-light-gray" />
-              <div>
-                <p className="mb-4 text-base font-medium text-main-gray">
-                  Years Experience
-                </p>
-                <p className="text-4xl font-bold text-main-dark-blue">4</p>
-              </div>
-              <div className="h-20 border-l-2 border-main-light-gray" />
-              <div>
-                <p className="mb-4 text-base font-medium text-main-gray">
-                  Happy Clients
-                </p>
-                <p className="text-4xl font-bold text-main-dark-blue">70+</p>
-              </div>
-            </div>
-          </div>
-          <div className="col-span-6 flex justify-end">
-            <img
-              src="/homepage-illustration.svg"
-              alt="Homepage illutration"
-              className="w-full max-w-[636px]"
-            />
-          </div>
-        </div>
-
-        {/* SPLIT */}
-
+        <Hero />
         <Services />
+        <Projects />
+        <CTA />
       </main>
 
       <Footer />
+    </div>
+  );
+}
+
+function Hero() {
+  return (
+    <div className="wrapper grid grid-cols-12 items-center justify-between gap-5 pb-28 pt-32">
+      <div className="col-span-5 col-start-2">
+        <h1 className="mb-[34px] text-[64px] font-bold leading-tight text-main-dark-blue">
+          Hello! I Am
+          <br />
+          Bilal Arve.
+          <br />I Am an Illustrator
+          {/* <span className="rounded-full bg-main-purple py-3 px-6 text-2xl font-bold text-white">
+                Illustrator
+              </span> */}
+        </h1>
+        <p className="mb-[50px] max-w-[466px] text-base leading-relaxed text-main-gray">
+          Hey Im Muhammad Bilal Arve and i’m from Indonesia.I have been working
+          as an Illustrator and Graphic Designer with more than 3 years of
+          experience and UI UX Designer for 1 years and still learning and
+          exploring.
+        </p>
+        <div className="mb-14 flex gap-[34px]">
+          <Button text="Lets Talk" theme="fill-blue" icon={<HiChat />} />
+          <Button
+            text="Resume"
+            theme="border-dark-blue"
+            icon={<HiClipboardList />}
+          />
+        </div>
+        <div className="flex gap-6">
+          <div>
+            <p className="mb-4 text-base font-medium text-main-gray">
+              Projects Completed
+            </p>
+            <p className="text-4xl font-bold text-main-dark-blue">300+</p>
+          </div>
+          <div className="h-20 border-l-2 border-main-light-gray" />
+          <div>
+            <p className="mb-4 text-base font-medium text-main-gray">
+              Years Experience
+            </p>
+            <p className="text-4xl font-bold text-main-dark-blue">4</p>
+          </div>
+          <div className="h-20 border-l-2 border-main-light-gray" />
+          <div>
+            <p className="mb-4 text-base font-medium text-main-gray">
+              Happy Clients
+            </p>
+            <p className="text-4xl font-bold text-main-dark-blue">70+</p>
+          </div>
+        </div>
+      </div>
+      <div className="col-span-6 flex justify-end">
+        <div className="w-full max-w-[636px]">
+          <Image
+            src="/homepage-illustration.svg"
+            alt="Homepage illutration"
+            layout="responsive"
+            width={631}
+            height={636}
+          />
+        </div>
+      </div>
     </div>
   );
 }
@@ -126,6 +135,117 @@ function Services() {
             description="Visual Identity, Poster, Stationary, Social Media, Infographics"
             backgroundColor="bg-main-cyan"
             src="/icon/graphic-design.svg"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Projects() {
+  const data = [
+    {
+      name: "Weather App",
+      src: "/projects/weather-app.png",
+      category: "UI/UX Design",
+      url: "",
+    },
+    {
+      name: "KRL Access",
+      src: "/projects/krl-access-app.png",
+      category: "UI/UX Design",
+      url: "",
+    },
+    {
+      name: "Podcast App",
+      src: "/projects/podcast-app.png",
+      category: "UI/UX Design",
+      url: "",
+    },
+    {
+      name: "Enjoying Illustration",
+      src: "/projects/enjoying-llustration.png",
+      category: "Illustration",
+      url: "",
+    },
+    {
+      name: "Cleaning App",
+      src: "/projects/cleaning-app.png",
+      category: "UI/UX Design",
+      url: "",
+    },
+    {
+      name: "Enjoy Working",
+      src: "/projects/enjoy-working.png",
+      category: "Illustration",
+      url: "",
+    },
+  ];
+
+  return (
+    <div className="mt-40">
+      <div className="wrapper">
+        <div className="grid grid-cols-12 gap-5">
+          <div className="col-span-11 col-start-2 mb-10">
+            <p className="mb-3.5 text-xs font-bold tracking-[8px] text-main-gray">
+              SERVICES
+            </p>
+            <h4 className="text-5xl font-bold text-main-dark-blue">
+              What can i do?
+            </h4>
+          </div>
+        </div>
+
+        <div className="grid grid-cols-2 gap-5">
+          {data.map((item) => (
+            <div
+              className="group relative h-[432px] w-full overflow-hidden rounded-[20px] bg-red-500"
+              key={item.name}
+            >
+              <div className="flex-center invisible absolute inset-0 z-10 flex-col gap-7 bg-gradient-to-bl from-[#403f63a5] to-[#0c0c20cc]  group-hover:visible">
+                <h5 className="text-2xl font-bold text-white">{item.name}</h5>
+                <h6 className="text-base font-semibold text-white">
+                  {item.category}
+                </h6>
+                <Button text="View Project" theme="transparent-white" />
+              </div>
+              <Image
+                className="h-full w-full object-cover"
+                src={item.src}
+                alt={item.name}
+                layout="fill"
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function CTA() {
+  return (
+    <div className="mt-80 mb-[140px]">
+      <div className="wrapper relative overflow-hidden rounded-[20px] bg-main-dark-blue">
+        <div className="py-24 px-[120px]">
+          <h2 className="mb-10 text-5xl font-bold text-white">
+            interested? Let&apos;s talk about the
+            <br />
+            project and make it happen
+          </h2>
+          <Button
+            text="Let Start it"
+            theme="transparent-white"
+            className="px-10"
+          />
+        </div>
+
+        <div className="absolute right-0 bottom-0 top-10 w-[505px]">
+          <Image
+            src="/homepage-cta.svg"
+            alt="Homepage CTA Illustration"
+            layout="fill"
+            objectFit="contain"
           />
         </div>
       </div>
